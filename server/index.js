89cdbefb1877395908fcb2f81fd3d6319551f09c
Farjana Akter <fakter@xpanxion.com>
2023-06-07 18:03:01 -0400

    update 3

--- server/index.js
+++ server/index.js
@@ -17,25 +17,59 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Your endpoints here..
+//example 1
 
 app.get("/message", cors(corsOptions), async (req, res) => {
   res.send({ message: "Hello World to meee" });
 });
-
+//eaxmple2
 app.get("/cars/:id", cors(corsOptions), async (req, res) => {
-  //const carId = req.params["id"];
+  const carId = req.params["id"];
 
-  // const result = await promisePool.query(`SELECT * FROM car WHERE car_id = ?`, [
-
-  //   carId,
-  // ]);
-  const result = await promisePool.query(`SELECT * FROM car WHERE car_id = 1`)
- if (result.error) 
- console.log (error.message)
+  const result = await promisePool.query(`SELECT * FROM car WHERE car_id = ?`, [
+    carId,
+  ]);
+  if (result.error) console.log(error.message);
   const body = result[0];
   res.send(body);
 });
 
+//example3
+// app.get('/cars?:make', cors(corsOptions), async (req, res) =>{
+//  // app.get('/cars', cors(corsOptions), async (req, res) =>{
+// const carMake = req.params.make;
+
+// const result = await promisePool.query(`SELECT * FROM car WHERE  make =?`, [carMake,]);
+// //const result = await promisePool.query(`SELECT * FROM car WHERE make = Ford`)
+// if (result.error) console.log(error.message);
+  
+// const body = result;
+// res.send(body);
+
+// });
+
+
+app.get('/cars', cors(corsOptions), async (req, res) => {
+
+    const carMake = req.query['make'];
+
+    const [result] = await promisePool.query(
+
+        `SELECT * FROM car WHERE make = ? `,
+
+        [carMake]
+
+    );
+
+    //const body = [result];
+    const body = result;
+    res.send(body);
+
+});
+
+
+
 app.listen(8080, () => {
   console.log(`Express web API running on port: ${PORT}.`);
 });
+
