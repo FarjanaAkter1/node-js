ae2f6fc5f5dc95bc13223d89c129c37595e3407c
Farjana Akter <fakter@xpanxion.com>
2023-06-07 15:40:21 -0400

    update

--- server/index.js
+++ server/index.js
@@ -1,30 +1,41 @@
-const cors = require('cors')
-const express = require('express')
-const { body, check, param, validationResult } = require('express-validator')
+const cors = require("cors");
+const express = require("express");
+const { body, check, param, validationResult } = require("express-validator");
 
-const PORT = 3000
-const app = express()
-const corsOptions = { origin: ['http://localhost:3000'], optionsSuccessStatus: 200 }
+const promisePool = require("../promisePool").Pool;
+
+const PORT = 8080;
+const app = express();
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  optionsSuccessStatus: 200,
+};
 
 // Middleware...
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Your endpoints here..
 
-//app.get('/person/:id', cors(corsOptions), async (req, res) => { 
-    app.get('/message', cors(corsOptions), async (req, res) => {
-    // const result = await mySqlProxy.<YOUR FUNCTION HERE>
-    // const id = req.params['id']                  // Read parameters from URL.
-    // const personType = req.query['personType']   // Read query parameters from URL.
-    // const body = req.body                        // Read request body.
-    // res.send(<YOUR OBJECT HERE>)
-    res.send({message: 'Hello World to meee'})
-})
+app.get("/message", cors(corsOptions), async (req, res) => {
+  res.send({ message: "Hello World to meee" });
+});
 
+app.get("/cars/:id", cors(corsOptions), async (req, res) => {
+  //const carId = req.params["id"];
 
+  // const result = await promisePool.query(`SELECT * FROM car WHERE car_id = ?`, [
 
-app.listen(3000, () => {
-    console.log(`Express web API running on port: ${PORT}.`)
-})
+  //   carId,
+  // ]);
+  const result = await promisePool.query(`SELECT * FROM car WHERE car_id = 1`)
+ if (result.error) 
+ console.log (error.message)
+  const body = result[0];
+  res.send(body);
+});
+
+app.listen(8080, () => {
+  console.log(`Express web API running on port: ${PORT}.`);
+});
